06e4da0aa2e544a1aa7940790bd4a14d3eed4300
connkat <hi.connkat@gmail.com>
2022-06-28 09:14:03 -0700

    kind of a watermark

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -6,8 +6,11 @@ function HomePage() {
       <div className="left">
         <h2>Does it even exist???</h2>
       </div>
-      <div className="img-wrapper">
-        <img src={fwf} alt="darcy" className="darcy" />
+      <div className="center">
+        <div className="img-wrapper">
+          <img src={fwf} alt="darcy" className="darcy" />
+        </div>
+        <h4>(don't worry FWF pals, I promise to make you a real website)</h4>
       </div>
       <div className="right">
         <h2>Why no ring???</h2>
